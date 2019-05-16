dc9d8a4fd07b699748c423fe5bd88fd99c9e1f95
David Cantu Martinez <davidcantu966@gmail.com>
2019-05-16 16:53:50 -0500

    Added registrations module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :username, :first_name, :last_name)}
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :username, :first_name, :last_name)}
+        end
 end
