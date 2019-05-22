052f5d75ef8a8a376680b087c305f0ceb3f6ee1c
David Cantu Martinez <davidcantu966@gmail.com>
2019-05-22 12:53:36 -0500

    fix sendgrid username

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :domain => 'yourdomain.com',
-    :user_name => 'davidcantum',
-    :password => 'Dcm181214',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :login,
     :enable_starttls_auto => true
   }
